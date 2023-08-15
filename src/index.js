27b87264136c3c4bf559b6c590263ed0c4dc6c7b
Iryna Burlaka <i.am.blinova@gmail.com>
2023-08-15 15:40:57 +0200

    frameworks instal

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+import SlimSelect from 'slim-select';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import 'slim-select/dist/slimselect.css';
 
 const refs = {
   select: document.querySelector('.breed-select'),
@@ -6,33 +10,71 @@ const refs = {
   error: document.querySelector('.error'),
   div: document.querySelector('.cat-info'),
 };
-console.log(refs.select);
-console.log(refs.loader);
-console.log(refs.error);
 
-window.onload = event => {
+// refs.loader.classList.add('is-hidden', 'loader');
+// refs.select.classList.replace('is-hidden');
+// refs.error.classList.replace('is-hidden');
+
+window.onload = () => {
+  Notify.failure('Qui timide rogat docet negare');
+  // refs.loader.classList.replace('is-hidden', 'loader');
+  refs.select.classList.add('is-hidden');
+  // refs.error.classList.replace('is-hidden');
+
+  //   const catSelect = new SlimSelect({
+  //     select: refs.select,
+  //   });
   fetchBreeds()
-    .then(data => renderIdList(data.data))
+    .then(res => {
+      const array = [
+        {
+          value: 'null',
+          text: 'Select a cat',
+          placeholder: true,
+        },
+      ];
+      res.data.forEach(el => {
+        array.push({ text: el.name, value: el.id });
+      });
+      console.log(array);
+      new SlimSelect({
+        select: refs.select,
+        data: array,
+        // settings: {
+        //   hideSelected: true,
+        // },
+      });
+
+      //   renderIdList(data.data);
+    })
     .catch(error => console.log(error));
 };
 
-function renderIdList(animalList) {
-  let markup = '';
-  for (const cat of animalList) {
-    markup += `<option value="${cat.id}">${cat.name}</option>`;
-  }
-  refs.select.innerHTML = markup;
-}
+// function renderIdList(animalList) {
+//   let markup = '';
+//   for (const cat of animalList) {
+//     markup += `<option value="${cat.id}">${cat.name}</option>`;
+//   }
+//   refs.select.innerHTML = markup;
+// }
 
 refs.select.addEventListener('change', showCatCard);
 
 function showCatCard(e) {
-  fetchCatByBreed(e.currentTarget.value)
-    .then(data => renderCatInfo(data))
-    .catch(error => console.log(error));
+  //   refs.loader.classList.replace('is-hidden', 'loader');
+  //   refs.select.classList.add('is-hidden');
+  //   refs.error.classList.add('is-hidden');
+  const breedId = e.currentTarget.value;
+  if (breedId != 'null') {
+    fetchCatByBreed(breedId)
+      .then(data => renderCatInfo(data))
+      .catch(error => console.log(error));
+  }
 }
 
-function renderCatInfo() {
-  let catCardInfo = `<img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p>`;
-  refs.div.innerHTML = catCardInfo;
+function renderCatInfo(catData) {
+  console.log(catData);
+  const { url, breeds } = catData.data[0];
+  const { name, description, temperament } = breeds[0];
+  refs.div.innerHTML = `<div class="box-img"><img src="${url}" alt="${name}" width="400"/></div><div class="box"><h1>${name}</h1><p>${description}</p><p><b>Temperament:</b> ${temperament}</p></div>`;
 }
